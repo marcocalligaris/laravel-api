4512646390c90742c6c79f66ebc4348be594abaa
marcocalligaris <marcocally@gmail.com>
2022-10-04 14:45:43 +0200

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="content">
+        <Placeholder :title='title'/>
+    </div>
+</template>
+
+<script>
+import Placeholder from './Placeholder.vue';
+
+    export default {
+    name: "App",
+    components: { Placeholder },
+    data() {
+            return {
+                title: 'Work in progress',
+            };
+        },
+};
+</script>
